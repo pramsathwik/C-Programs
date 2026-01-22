86f536565b9a7e41b3863ab65a95d6b64641741d
PULI RAM SATHWIK <pramsathwik7@gmail.com>
2026-01-22 22:25:13 +0530

    Add files via upload

---
+++ simple.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str1[50], str2[50];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str1, 50, stdin);
+
+    while (str1[i] != '\0') {
+        str2[i] = str1[i];
+        i++;
+    }
+    str2[i] = '\0';
+
+    printf("Copied string: %s", str2);
+    return 0;
+}
