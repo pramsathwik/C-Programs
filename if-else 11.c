ca3fd48dcc05f9c7761af522b911f4d7aa0e3957
PULI RAM SATHWIK <pramsathwik7@gmail.com>
2026-03-12 23:29:20 +0530

    Add files via upload

---
+++ if-else 11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=-9&&a<=9)
+    {
+        printf(" Single Digit");
+    }
+    else
+    {
+        printf("  Not Single Digit");
+    }
+    return 0;
+}
+    
